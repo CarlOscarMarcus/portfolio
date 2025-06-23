58d5817bb2ecec562ee5aef50907234dd2284442
CarlOscarMarcus <oscar@wysarna.se>
2025-06-23 10:18:07 +0200

    Added TechStack Slider and fixed Translation bug

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Hero from "./Hero.jsx";
+import TechSlider from "./animations/TechSlider.js";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 
@@ -8,6 +9,7 @@ const Layout = () => (
 	<div className="app">
 		<Header />
 		<Hero />
+		<TechSlider />
 		<main>
 			<Outlet />
 		</main>
---
+++ src/components/animations/TechSlider.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHtml5,
+  faCss3Alt,
+  faJsSquare,
+  faPython,
+  faDocker,
+  faReact,
+  faSymfony,
+  faGithub,
+  faGitAlt,
+  faPhp,
+  faLinux,
+} from '@fortawesome/free-brands-svg-icons';
+
+const technologies = [
+    { name: 'HTML5', icon: faHtml5 },
+    { name: 'CSS3', icon: faCss3Alt },
+    { name: 'JavaScript', icon: faJsSquare },
+    { name: 'PHP', icon: faPhp },
+    { name: 'Python', icon: faPython },
+    { name: 'React', icon: faReact },
+    { name: 'Symfony', icon: faSymfony },
+    { name: 'GitHub', icon: faGithub },
+    { name: 'Git', icon: faGitAlt },
+    { name: 'Docker', icon: faDocker },
+    { name: 'Linux', icon: faLinux },
+];
+
+const TechSlider = () => {
+  return (
+    <div className="tech-slider">
+      <div className="tech-slider__track">
+        {[...technologies, ...technologies].map((tech, index) => (
+          <div className="tech-slider__item" key={index}>
+            <FontAwesomeIcon icon={tech.icon} size="2x" />
+            <span className="tech-slider__label">{tech.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TechSlider;
